0b2c71bf54c5d1aac78f87c84247504f9ac21c02
Jeremiah Russell <jrussell@jerus.ie>
2021-10-29 08:12:17 +0100

    feat: Prettier output and capture of question
    Problem
    No context for the answer
    Solution
    - capture the question
    - provide question as output
    - Use formatting to distinguish
    Note

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,37 @@
+use colored::Colorize;
 use m8b::MagicBall;
+use std::env;
 
 fn main() {
     let magic_ball = MagicBall::new();
 
-    println!("The magic eight ball says: {}", magic_ball.shake());
+    println!("{} {}", "Question:".blue(), get_the_question().bold());
+
+    println!(
+        "{} {}",
+        "The magic eight ball says:".blue(),
+        magic_ball.shake().bold().purple()
+    );
+}
+
+fn get_the_question() -> String {
+    let args: Vec<String> = env::args().collect();
+    println!("{:?}", args);
+
+    let mut question = String::new();
+
+    for (index, arg) in args.iter().enumerate() {
+        match index {
+            0 => {}
+            1 => {
+                question.push_str(arg);
+            }
+            _ => {
+                question.push(' ');
+                question.push_str(arg);
+            }
+        }
+    }
+
+    question
 }
